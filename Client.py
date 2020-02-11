9322b57db00dc984af8b75c03ded71f5d8eed8a3
Johannes R. Falnes <johannes@falnes.no>
2020-02-11 19:12:48 +0100

    Working on CSV, Tuesday 19:12

--- Client.py
+++ Client.py
@@ -1,73 +1,116 @@
+# IMPORT
 from tkinter import *
 from tkinter import messagebox
 import socket
 import logging
 import threading
-
+# GLOBAL VARIABLES
+# VARIABLES
 HOST = "127.0.0.1"
 PORT = 60000
 BUFFER = 1024
 ADDR = (HOST, PORT)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-def add_item():
-    logging.info("Item added")
-    a = codeVar.get()
-    b = descVar.get()
-    c = amountVar.get()
-    abc = "{}:{}:{}".format(a, b, c)
-    logging.info(abc + " sent to server")
-    client_socket.send(bytes(abc, 'utf8'))
-    messagebox.showinfo("Item Added", abc)
-
-
-def conn_recv():
-    b = client_socket.recv(BUFFER).decode('utf8')
-    logging.info(b)
-
-
-def exit_def():
-    exit()
-
+# LOGGING
 
 logFormat = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logFormat, level=logging.INFO,
                     datefmt="%H:%M:%S")
 
 
+def conn_recv():
+    """for receiving buffer from server"""
+    client_recv = client_socket.recv(BUFFER).decode('utf8')
+    logging.info(client_recv)
+
+
+def exit_def():
+    """exits the program when the exit button is pressed"""
+    exit()
+
+
+def add_item():
+    """Send """
+    # Get the text from the entry fields
+    a = codeVar.get()
+    b = descVar.get()
+    c = amountVar.get()
+    logging.info("Item added")
+
+    # Concatenate the text from the entry fields into a single variable
+    abc = "{}:{}:{}".format(a, b, c)
+    logging.info(abc + " sent to server")
+
+    # Send abc to server,
+    client_socket.send(bytes(abc, 'utf8'))
+
+    # Messagebox showing which items were added
+    messagebox.showinfo("Item Added", abc)
+
+
+def update_item():
+    a = codeVar.get()
+    b = descVar.get()
+    c = amountVar.get()
+    client_socket.send(bytes(("@@updateitem?{}:{}:{}".format(a, b, c)), 'utf8'))
+
+
+def show_stock():
+    a = codeVar.get()
+    b = descVar.get()
+    c = amountVar.get()
+    """sends a request to the server to show the stock"""
+    client_socket.send(bytes('@@showstock?{}:{}:{}'.format(a, b, c), 'utf8'))
+
+
+def get_help():
+    """for displaying the menu option 'help'"""
+    messagebox.showinfo("Help", "")
+
+
 def tk_window():
+    """function that displays a tkinter gui"""
+    # Global variables
     global codeVar
     global descVar
     global amountVar
+
     window = Tk()
     window.title("Add a stock item to the server")
-    window.geometry("250x150")
+    window.geometry("500x500")
     window.resizable(False, False)
 
     codeVar = StringVar()
     descVar = StringVar()
     amountVar = StringVar()
 
-    entryCode = Entry(window, textvar=codeVar)
-    entryDesc = Entry(window, textvar=descVar)
-    entryAmount = Entry(window, textvar=amountVar)
+    entry_code = Entry(window, textvar=codeVar)
+    entry_desc = Entry(window, textvar=descVar)
+    entry_amount = Entry(window, textvar=amountVar)
+
+    help_menu = Menu(window)
+    help_menu.add_command(label="Help", command=get_help)
+    window.config(menu=help_menu)
 
     labelCode = Label(window, text="Code: ")
     labelDesc = Label(window, text="Description: ")
     labelAmount = Label(window, text="Amount: ")
     addBtn = Button(window, text="Add", relief="raised", command=add_item)
     exitBtn = Button(window, text="Exit", relief="raised", command=exit_def)
-
+    updateBtn = Button(window, text="Update", relief="raised", command=update_item)
+    showBtn = Button(window, text="Show Stock", relief="raised", command=show_stock)
     labelCode.grid(row=0, column=0)
     labelDesc.grid(row=1, column=0)
     labelAmount.grid(row=2, column=0)
-    entryCode.grid(row=0, column=1)
-    entryDesc.grid(row=1, column=1)
-    entryAmount.grid(row=2, column=1)
-
+    entry_code.grid(row=0, column=1)
+    entry_desc.grid(row=1, column=1)
+    entry_amount.grid(row=2, column=1)
     addBtn.grid(row=3, column=1)
     exitBtn.grid(row=3, column=2)
+    updateBtn.grid(row=3, column=0)
+    showBtn.grid(row=3, column=3)
+
     window.mainloop()
 
 
--- Main.py
+++ Main.py
@@ -10,15 +10,27 @@ class StockItem:
         self.description = description
         self.amount = amount
 
-    def return_item(self):
-        self.itemDesc = "{}, {}, {}".format(self.code, self.description, self.amount)
-
-
 
 class StockTracker(StockItem):
-    def __init__(self,  code, description, amount):
+    def __init__(self, code, description, amount):
         super().__init__(code, description, amount)
         self.amount = amount
+
+
+def write_item(code, desc, amount):
+    in_list = [code, desc, amount]
+    with open('stock.csv', 'a', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=";")
+        writer.writerow(in_list)
+
+
+def command_line():
+    while True:
+        print("What do you want to do?\n1.Add a stock item\n2.Update stock item\n3.Display details of item\n4.Display "
+              "the entire stock list\n5.Exit")
+        user_input = input("Please enter your selection: \n")
+        if user_input == "1":
+            print("Stock item added.")
 
 
 HOST = "127.0.0.1"
@@ -26,10 +38,7 @@ PORT = 60000
 BUFFER = 1024
 addresses = {}
 
-
 a = StockItem(1, 2, 3)
-
-
 logFormat = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logFormat, level=logging.INFO,
                     datefmt="%H:%M:%S")
@@ -38,35 +47,66 @@ logging.basicConfig(format=logFormat, level=logging.INFO,
 def incoming_connection():
     global addr
     while True:
-        conn, addr = serv_socket.accept()
+        conn, addr = socket_serv.accept()
         addresses[conn] = addr
         logging.info(f'Connection established with {str(conn)}')
-        conn.sendall(bytes(('Connection Established with ' + str(addr)),  'utf8'))
-        t3 = threading.Thread(target=handle_conn, args=(conn,))
-        t3.start()
+        conn.sendall(bytes(('Connection Established with ' + str(addr)), 'utf8'))
+        thread_handle = threading.Thread(target=handle_conn, args=(conn,))
+        thread_handle.start()
 
 
-def handle_conn(client):
-    while True:
-        coderecv = client.recv(BUFFER).decode('utf8')
+def csv_edit(coderecv):
+    """manipulates stocks.csv depending on the buffer sent from the client"""
+    r = csv.reader(open('stock.csv', ), delimiter=";")
+    lines = list(r)
+    if "@@updateitem?" in coderecv:
+        y = coderecv.split("?")
+        item = y[1]
+        print(item)
+        item.split(":")
+        new_code = item[0]
+        new_desc = item[1]
+        new_amount = item[2]
+        print(new_code, new_desc, new_amount)
+        for line_number in range(len(lines)):
+            csvList = lines[line_number]
+            if csvList[0] == item:
+                print(lines[line_number])
+
+    elif "@@showstock:" in coderecv:
+        y = coderecv.split(":")
+        item = y[1]
+        for line_number in range(len(lines)):
+            csvList = lines[line_number]
+            if csvList[0] == item:
+                print(lines[line_number])
+    else:
         x = coderecv.split(":")
         code = x[0]
         desc = x[1]
         amount = x[2]
-        print(code, desc, amount)
 
         logging.info(str(addr) + " sent: " + coderecv)
-        with open('sample.txt', 'a+') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([code, desc, amount])
+
+        write_item(code, desc, amount)
 
 
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 / TCP
-serv_socket.bind((HOST, PORT))
-serv_socket.listen(5)
+def handle_conn(client):
+    """Handles input from client to server"""
+
+    while True:
+        coderecv = client.recv(BUFFER).decode('utf8')
+        csv_edit(coderecv)
+
+
+socket_serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4 / TCP
+socket_serv.bind((HOST, PORT))
+socket_serv.listen(5)
 logging.info("Waiting for connection...")
 
-serv_thread = threading.Thread(target=incoming_connection)
-serv_thread.start()
-serv_thread.join()
-serv_socket.close()
+thread_cli = threading.Thread(target=command_line)
+thread_cli.start()
+thread_serv = threading.Thread(target=incoming_connection)
+thread_serv.start()
+thread_serv.join()
+socket_serv.close()
